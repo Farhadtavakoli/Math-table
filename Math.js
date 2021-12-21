b17a1a08a47029d6c3043542316e466bdc3111d6
Farhad Tavakoli <72437846+Farhadtavakoli@users.noreply.github.com>
2021-12-21 12:55:14 +0100

    Fix a bug

---
+++ Math.js
@@ -0,0 +1,45 @@
+// If we click on the start/reset button,
+let playing = false;
+let score = 0;
+let time = 61;
+document.getElementById("time").hidden = true;
+document.getElementById("startReset").onclick = function () {
+  console.log("I do it!");
+  if (playing) {
+    location.reload();
+  } else if (!playing) {
+    console.log("Not playing");
+    playing = true;
+    score = 0;
+    document.getElementById("scoreValue").innerHTML = score;
+    document.getElementById("time").hidden = false;
+    document.getElementById("startReset").innerHTML = "Reset";
+    while (time != 0) {
+      time = time - 1;
+      document.getElementById("remain").innerHTML = time;
+    }
+  }
+};
+
+/*
+      if we are playing 
+         reload the page 
+       if we are not playing 
+         set score to zero
+         show countdowm box
+         reduce time by 1sec ina loop
+             time left? 
+                yes--> countinue
+                else Gameover
+         change the button to reset
+         generate a new question and multiple answers */
+
+/* If we click on the answer box
+      if we are playing
+         correct? 
+            yes: increase score
+                 show correct box
+                 generate new question
+                    
+            no: 
+                show try again box for 1 sec*/
